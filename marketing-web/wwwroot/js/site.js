7bc5fcec2ae728040e1d153746f0bbc87a04bd2a
gentritkrasniqi <gk44739@ubt-uni.net>
2021-07-15 20:21:37 +0200

    Responsive

--- marketing-web/wwwroot/js/site.js
+++ marketing-web/wwwroot/js/site.js
@@ -9,4 +9,18 @@
       prevEl: ".swiper-button-prev",
     },
   });
+
+  $('.hamburgerMenu').on('click',function(e) {
+
+    if($(e.currentTarget).hasClass('active')){
+      $(e.currentTarget).removeClass('active');
+      $(e.currentTarget).siblings('nav').removeClass('active');
+    }else{
+      $(e.currentTarget).addClass('active');
+      $(e.currentTarget).siblings('nav').addClass('active');
+    }
+    
+
+  });
+
 });
