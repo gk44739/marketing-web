eeeada470170e48f22b90442e4533e0802d95905
gentritkrasniqi <gk44739@ubt-uni.net>
2021-07-16 20:47:24 +0200

    minor changes

--- marketing-web/wwwroot/js/site.js
+++ marketing-web/wwwroot/js/site.js
@@ -16,16 +16,25 @@
       $(e.currentTarget).removeClass('active');
       $(e.currentTarget).siblings('nav').removeClass('active');
       $("body").removeClass('navActive');
+      $(e.currentTarget).siblings('nav').children('ul').children().each(function (i,item) {
+        $(item).removeClass('aos-init aos-animate');
+      });
     }else{
       $(e.currentTarget).addClass('active');
       $(e.currentTarget).siblings('nav').addClass('active');
       $("body").addClass('navActive');
+      $(e.currentTarget).siblings('nav').children('ul').children().each(function (i,item) {
+        $(item).addClass('aos-init aos-animate');
+      });
     }
     
 
   });
 
   if($(window).width() < 992){
+    $(".linksNavbar").children().each(function (i,item) {
+      $(item).removeClass('aos-init aos-animate')
+    });
     $('.animate-box').each(function (i,item) {
       $(item).attr('data-aos-delay',100)
     });
