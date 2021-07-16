40cb2291be8cf5c6872cb46840603a1b24c19eab
dionk6 <kukadion1@gmail.com>
2021-07-16 20:48:27 +0200

    Package for runtime

--- marketing-web/Startup.cs
+++ marketing-web/Startup.cs
@@ -24,6 +24,7 @@ namespace marketing_web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
